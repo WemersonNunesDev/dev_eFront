b3ebe361696c21c7d1bb66bada768eebdf20ec63
WemersonNunesDev <wemersononunes@gmail.com>
2024-04-15 09:54:40 -0300

    Atualização
    Atualizei

--- jogador/src/script.js
+++ jogador/src/script.js
@@ -1,13 +1,6 @@
-function criar_ficha() {
-    const jogador = {
-        nome: "Wemerson",
-        personagem: "Vartax",
-        raca: "Bugbear",
-        classe: "Clérigo",
-        nivel: 5,
-    };
+const botao = document.querySelector('.btn-raca')
+const elementoHabilidade = document.querySelector('.btn-raca .txt-habilidade')
 
-    console.log(`${jogador.nome}`) //1° forma de monstrar o elemento
-    console.log(`${jogador["nome"]}`)//2° forma de monstrar o elemento
-}
-
+botao.addEventListener("click", () => {
+    elementoHabilidade.classList.toggle("ativo")
+})
